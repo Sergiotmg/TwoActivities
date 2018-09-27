2be457a84933258137b980d215cfe0695572a06c
Sergio <sergiotmg@gmail.com>
2018-09-27 13:47:07 +0200

    aplicacion 2 activities 1.0 27/09

--- app/src/main/java/com/example/sergio/twoactivities/Main2Activity.java
+++ app/src/main/java/com/example/sergio/twoactivities/Main2Activity.java
@@ -1,13 +1,53 @@
 package com.example.sergio.twoactivities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class Main2Activity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY ="com.example.android.twoactivities.extra.REPLY";
+
+    private TextView textMessage;
+    private EditText mReply;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        textMessage=findViewById(R.id.textMessage);
+        //recoger la variable de la act 1 main
+        Intent intent = getIntent();
+        //pongo main Activity  yel punto y se autocompleta con la que tenga en el main Activity
+        //String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        textMessage.setText(getIntent().getStringExtra(MainActivity.EXTRA_MESSAGE));
+        mReply=findViewById(R.id.editText_main);
+        // lo mostramos ahora en el textview
+
+
+    }
+
+
+
+    //metodo creado pulsando en el activity_main2 con create on clic eventhandler
+    // al hacer alt+ enter en
+    public void returnData(View view) {
+        String reply= mReply.getText().toString();
+        // no lo queremos para actividades nuevas y no le pasamos nada dentro del intent
+        Intent replyIntent = new Intent();
+        //ponerle de nombre a la variable y de valor lo del texto de respuesta
+        replyIntent.putExtra(EXTRA_REPLY,reply);
+        //volver a la actividad anterior
+        setResult(RESULT_OK,replyIntent);
+        //setResult(); solo usar cuando sea Cancel, al cancelar el login RESULT_CANCEL y no pasa dat
+        //setResult(RESULT_FIRST_USER,); SI HACE algo por 1ª vez el user
+
+        finish();
+
     }
 }
--- app/src/main/java/com/example/sergio/twoactivities/MainActivity.java
+++ app/src/main/java/com/example/sergio/twoactivities/MainActivity.java
@@ -5,27 +5,57 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;//Place the cursor on the word "Log" and press Alt-Enter 
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG =MainActivity.class.getSimpleName();
+    private static final String LOG_TAG =MainActivity.class.getSimpleName();// con eso no s da el nombre de la actividad
+    //PONER LOG_TAG EN MAYUS. es privada porque solo queremos utilizar en esta clase Main
+    public static final String EXTRA_MESSAGE = "message";
 
+    // se le puede llamar desde cualquier otra clase, static quire decir que esa variable siempre valdrá
+    //lo mismo. EJ: static int x=2, da igual todos los objetos distintos de esa clase(persona:p1p2p3) esa variable valdrá
+    // 2 en todas de ellas, si en una de ellas cambio de x=2 a x=4 ahora cambia en todas las personas x de 2 a 4
+    //el FINAL ES para declarar constantes, no se puede cambiar los valores una vez asignados , no puedo hacer
+    //EXTRA_MESSAGE ES OTRA COSA
+    //SIMPLEMENTE PONIENDO MainAc...EXTRA_MESSAGE  lo recojemos
+
+    private EditText mEditText; // se le pone m es por los programadores de android, quiere decir que esa variable es de esa instancia
+    // m que se ade tu clase entera
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //hacer esto siempre oOJO!!!!!
+        mEditText=findViewById(R.id.editText_main);//enlazo mi variable editrtet con la parte grafica
     }
 
+
     public void launchSecondActivity(View view) {
-        Log.d(LOG_TAG, "Botón presionado");
+        Log.e(LOG_TAG, "Botón presionado");
+        //Log.e("MAINACTIVITY", "ieeeeeeeeeeeeeeeeeeeeeeeeee");
         //con log que salgan por logcat
         //log.d d es de debug si es log.e será de error, mostradoen rojo
+        //PONER ESA LINEA!!! SIEMPRE
+        String message = mEditText.getText().toString();
+        //pasar esos valores de atras al intent:
+        Intent intent = new Intent(this, Main2Activity.class);
+
+
         //SIGUIENTE LINEA PARA ENVIAR ACTIVIDADES
-        Intent intent = new Intent(this, Main2Activity.class);//THIS PORQUE SIEMPRE
-        //PIDE CONTEXTO
-        startActivity(intent);// SE USA EN CLASES QUE EXTIENDAN DE ACTIVITY
+
+
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+        // SE USA EN CLASES QUE EXTIENDAN DE ACTIVITY
         //SI LA CASE NO EXTIENDE DE ACTIVITY NO SE PUEDE PONER lo anterior
         //startActivities() seria para mas de una a la vez
 
+
+    }
+    //para mostrar el mensaje del main2activity en el main activity 1
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
